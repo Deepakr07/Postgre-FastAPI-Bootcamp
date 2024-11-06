61a125c34321296c1f94842d7c713f42102632f1
Deepak <deepakrajesh567@gmail.com>
2024-11-06 15:33:30 +0900

    added two get end points for fetching data from questions table and choices table

--- models.py
+++ models.py
@@ -16,7 +16,7 @@ class Choices(Base):
 
     __tablename__ = 'choices'
 
-    id = Column(String,primary_key=True,index=True)
+    id = Column(Integer,primary_key=True,index=True)
     choice_text = Column(String,index=True)
     is_correct = Column(Boolean,default=False)
     question_id = Column(Integer,ForeignKey("questions.id"))
